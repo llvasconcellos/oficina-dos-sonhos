0ecef1b4237021e2349b14a806ea6e0f4e3ff01b
Leonardo Lima de Vasconcellos <leonardo@devhouse.com.br>
2016-09-09 01:18:56 -0300

    migrator config

---
+++ www/administrator/components/com_mtwmigrator/mtwmigrator_config.php
@@ -0,0 +1,26 @@
+<?php
+$mtwCFG['hostname'] = "localhost";
+$mtwCFG['dbname'] = "oficinaantigo";
+$mtwCFG['username'] = "root";
+$mtwCFG['password'] = "vertrigo";
+$mtwCFG['prefix'] = "jos_";
+$mtwCFG['ext_cb'] = "";
+$mtwCFG['ext_vm'] = "";
+$mtwCFG['ext_jc'] = "";
+$mtwCFG['ext_dm'] = "";
+$mtwCFG['ext_ff'] = "";
+$mtwCFG['ext_aj'] = "";
+$mtwCFG['ext_fb'] = "";
+$mtwCFG['backup'] = "0";
+$mtwCFG['groups'] = "1";
+$mtwCFG['users'] = "1";
+$mtwCFG['sections'] = "1";
+$mtwCFG['categories'] = "1";
+$mtwCFG['content'] = "1";
+$mtwCFG['frontpage'] = "0";
+$mtwCFG['menus'] = "0";
+$mtwCFG['modules'] = "0";
+$mtwCFG['polls'] = "1";
+$mtwCFG['weblinks'] = "1";
+$mtwCFG['contacts'] = "1";
+?>
